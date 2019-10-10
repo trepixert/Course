339d80fb6ccc8a2ce9e3b283423ee8ef1021c7fe
trepixert <pixert@mail.ru>
2019-10-10 12:28:46 +0400

    Добавьте файлы проекта.

---
+++ Lection/Calculator.h
@@ -0,0 +1,9 @@
+int sum(int first, int second);
+
+int minus(int first, int second);
+
+int multiply(int first, int second);
+
+int division(int first, int second);
+
+int factorial(int first);
---
+++ Lection/Main.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "Calculator.h"
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	char* locale = setlocale(LC_ALL,"");
+
+
+	int firstNumber;
+	int secondNumber;
+	char operation;
+	char status;
+
+
+
+	while (1) {
+		printf("Введите операцию: \n");
+		scanf(" %c", &operation);
+		printf("Введите первое число: \n");
+		scanf_s("%d", &firstNumber);
+		printf("Введите второе число: \n");
+		scanf_s("%d", &secondNumber);
+
+		printf("%d\n\n", doOperation(operation, firstNumber, secondNumber));
+
+		printf("Завершить работу? y/n \n");
+		scanf(" %c", &status);
+
+		if (status == 'y') break;
+	}
+	return 0;
+}
+
+int doOperation(char operation, int first, int second) {
+	switch (operation) {
+		case '+':
+			return sum(first,second);
+		case '-':
+			return minus(first, second);
+		case '*':
+			return multiply(first, second);
+		case '/':
+			return division(first, second);
+		case '!':
+			return factorial(first);
+	}
+}
